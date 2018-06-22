8af319de4645fa7db67d94f5319144dc9a502951
d1a9x8u9 <daxu7803@gmail.com>
2018-06-22 15:09:29 -0700

    fixed h scroll bug

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import './Post.css'
 const post = (props) => (
     <Card className="my-2 Card">
         <CardText className="px-2 CardText">{props.post.message}</CardText>
-        {props.post.imgDownloadURL ? <CardImg top width="100%" src={props.post.imgDownloadURL} alt='img.jpg' /> : null}
+        {props.post.imgDownloadURL ? <CardImg top width="95%" src={props.post.imgDownloadURL} alt='img.jpg' /> : null}
         <div className="px-2 text-right">
             <i className="far fa-thumbs-up"></i>
             <i className="far fa-heart pl-2"></i>
@@ -14,7 +14,6 @@ const post = (props) => (
         <CardBody className="pt-2 pb-1 px-1 text-right">
             <CardSubtitle className="CardSubtitle">{props.post.date}&nbsp;{props.post.time}</CardSubtitle>
             <CardTitle className="mb-1 CardTitle">{props.post.author}</CardTitle>
-            {/* <Button>Button</Button> */}
         </CardBody>
     </Card>
 )
